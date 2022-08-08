6f47de7f62a457d0f408c1ac006f79fd271caade
h.ayabe <h.ayabe_biz@live.jp>
2022-08-08 15:25:22 +0900

    fix analytics tag

--- company-site/pages/_document.tsx
+++ company-site/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -9,17 +8,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-P626NSEPMZ"
-            strategy="beforeInteractive"
-          ></Script>
-          <Script
-            type="text/javascript"
-            src="/script/analytics.js"
-            strategy="beforeInteractive"
-          ></Script>
           <div id="fb-root"></div>
           <div id="fb-customer-chat" className="fb-customerchat"></div>
         </body>
