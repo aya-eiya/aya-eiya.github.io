98c7f9847e11c04c6421133e0222a968c4208701
h.ayabe <h.ayabe_biz@live.jp>
2023-07-30 15:04:22 +0900

    remove old method

--- company-site/app/[page]/page.tsx
+++ company-site/app/[page]/page.tsx
@@ -2,17 +2,7 @@
 
 import { ReactElement } from 'react'
 import HomePage from '../HomePage'
-import { PageNames } from '../domains/pages'
 
 export default function Page(): ReactElement {
   return HomePage()
-}
-
-export async function getStaticPaths() {
-  return {
-    paths: PageNames.map((page) => ({
-      params: { page },
-    })),
-    fallback: true, // false or 'blocking'
-  }
 }
